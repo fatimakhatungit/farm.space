e271d75cc2593767796f143d35efbc7a231a5c39
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-04-18 01:42:19 +0600

    added

--- js/main.js
+++ js/main.js
@@ -5,6 +5,149 @@
 
 
 
+document.getElementById('button').addEventListener("click", function(){
+    document.querySelector(".fDQpfJ").style.display = "flex";
+})
+document.querySelector(".close").addEventListener("click", function(){
+   document.querySelector(".fDQpfJ") .style.display = "none";
+})
+
+
+document.getElementById('button-NYjZE').addEventListener("click", function(){
+    document.querySelector(".fDQpfJ").style.display = "flex";
+})
+document.querySelector(".close").addEventListener("click", function(){
+   document.querySelector(".fDQpfJ") .style.display = "none";
+})
+
+
+    document.getElementById('button-kmwRJx').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    
+
+
+    document.getElementById('button-price').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    
+
+    document.getElementById('button-wbnb').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    
+
+
+    document.getElementById('dllnUP').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    
+
+
+    document.getElementById('dllnUP').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+
+    document.getElementById('button-1').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    document.getElementById('button-2').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    document.getElementById('button-3').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    document.getElementById('button-4').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    document.getElementById('button-5').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    document.getElementById('button-6').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    document.getElementById('button-8').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    document.getElementById('button-7').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+
+    document.getElementById('button-7').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+
+    document.getElementById('button-9').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+
+    document.getElementById('button-10').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+
+    document.getElementById('button-11').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+
+    document.getElementById('button-12').addEventListener("click", function(){
+        document.querySelector(".fDQpfJ").style.display = "flex";
+    })
+    document.querySelector(".close").addEventListener("click", function(){
+       document.querySelector(".fDQpfJ") .style.display = "none";
+    })
+    
+
 
 
 })(jQuery);	
